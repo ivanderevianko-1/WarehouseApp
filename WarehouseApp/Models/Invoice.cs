9a31afc49c65edea19025e41345034210c979036
Деревянко Іван <ivan.derevianko@nure.ua>
2026-06-02 12:28:14 +0300

    Add project files.

---
+++ WarehouseApp/Models/Invoice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseApp.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; } = "";
+
+        public DateTime Date { get; set; }
+
+        public string ProductName { get; set; } = "";
+
+        public int Quantity { get; set; }
+
+        public string Comment { get; set; } = "";
+    }
+}
---
+++ WarehouseApp/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public string Unit { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime LastDeliveryDate { get; set; }
+    }
+}
