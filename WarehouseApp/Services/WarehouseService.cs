49a1c54acdbbdc8fe302a3c1429a1a942d7d9fa1
Деревянко Іван <ivan.derevianko@nure.ua>
2026-06-02 13:35:23 +0300

    Implement basic warehouse operations

---
+++ WarehouseApp/Services/WarehouseService.cs
@@ -0,0 +1,38 @@
+﻿using WarehouseApp.Models;
+
+namespace WarehouseApp.Services
+{
+    public class WarehouseService
+    {
+        public List<Product> Products { get; set; }
+
+        public List<Invoice> Invoices { get; set; }
+
+        public WarehouseService()
+        {
+            Products = new List<Product>();
+            Invoices = new List<Invoice>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+
+        public bool RemoveProduct(int id)
+        {
+            Product? product = FindProductById(id);
+
+            if (product == null)
+                return false;
+
+            Products.Remove(product);
+            return true;
+        }
+
+        public Product? FindProductById(int id)
+        {
+            return Products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
